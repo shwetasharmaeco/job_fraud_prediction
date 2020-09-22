2fbf06140dd64cfd0380cca724a435fdf54464f9
Shweta Sharma <shwetasharmaeco@gmail.com>
2020-09-22 00:15:20 -0700

    app main page

---
+++ static/js/style.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Update  Color
+
+
+
+var list = document.getElementsByTagName("td");
+
+for (const elem of list) {
+    // console.log(elem.innerHTML);
+    // console.log(elem.style);
+    if (elem.innerHTML === "Fraud"){
+      elem.style.color = "black";
+      elem.style.backgroundColor="red";
+    }
+    else if (elem.innerHTML === "Not fraud"){
+      elem.style.color= "white";
+      elem.style.backgroundColor = "green";
+    }}
